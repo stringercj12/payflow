525c212a0421c9cae8b542805fc0bc212b671b7a
Jefferson Ferreira <jefferson14489@gmail.com>
2022-04-03 20:27:12 -0300

    Animação no login

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -32,52 +32,63 @@ class _LoginPageState extends State<LoginPage> {
               height: size.height * 0.36,
               color: AppColors.primary,
             ),
-            AnimatedCard(
-              direction: AnimatedCardDirection.top,
+            Container(
               child: Positioned(
                 top: 40,
-                left: 0,
-                right: 0,
-                child: Image.asset(
-                  AppImages.person,
-                  width: 208,
-                  height: 300,
+                left: 90,
+                right: 90,
+                child: AnimatedCard(
+                  direction: AnimatedCardDirection.top,
+                  duration: const Duration(milliseconds: 400),
+                  child: Image.asset(
+                    AppImages.person,
+                    width: 208,
+                    height: 300,
+                  ),
                 ),
               ),
             ),
-            AnimatedCard(
-              direction: AnimatedCardDirection.left,
-              child: Positioned(
-                bottom: size.height * 0.05,
-                left: 0,
-                right: 0,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Image.asset(AppImages.logomini),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 70, right: 70),
-                      child: Text(
-                        "Organize seus boletos em um só lugar",
-                        textAlign: TextAlign.center,
-                        style: TextStyles.titleHome,
+            Positioned(
+              bottom: size.height * 0.05,
+              left: 0,
+              right: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  AnimatedCard(
+                    direction: AnimatedCardDirection.left,
+                    duration: const Duration(milliseconds: 500),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          AppImages.logomini,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 70, right: 70),
+                          child: Text(
+                            "Organize seus boletos em um só lugar",
+                            textAlign: TextAlign.center,
+                            style: TextStyles.titleHome,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  AnimatedCard(
+                    direction: AnimatedCardDirection.bottom,
+                    duration: const Duration(milliseconds: 600),
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.only(left: 40, right: 40, top: 70),
+                      child: SocialLoginButton(
+                        onTap: () {
+                          controller.googleSignIn(context);
+                        },
                       ),
                     ),
-                    AnimatedCard(
-                      direction: AnimatedCardDirection.right,
-                      child: Padding(
-                        padding:
-                            const EdgeInsets.only(left: 40, right: 40, top: 70),
-                        child: SocialLoginButton(
-                          onTap: () {
-                            controller.googleSignIn(context);
-                          },
-                        ),
-                      ),
-                    )
-                  ],
-                ),
+                  )
+                ],
               ),
             ),
           ],
